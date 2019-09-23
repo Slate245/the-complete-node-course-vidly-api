eb31e7d659bd2b2cda7d5d90fc5f086d6dcbcbbf
Ivan Bespalov <iebespalov@gmail.com>
2019-09-23 14:43:19 +0500

    Exercise - Extracting the Config Logic (My Solution)

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const config = require("config");
 const Joi = require("@hapi/joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
 require("./startup/logging")();
+require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

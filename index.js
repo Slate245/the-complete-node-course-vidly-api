1db10daa58b1823fd67ceeb54fa0454335cc391b
Ivan Bespalov <iebespalov@gmail.com>
2019-09-23 14:48:11 +0500

    Exercise - Extracting the Validation Logic (My Solution)

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Joi = require("@hapi/joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
@@ -7,6 +5,7 @@ require("./startup/logging")();
 require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
